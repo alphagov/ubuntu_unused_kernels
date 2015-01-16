51e61bc15e4d59870b5b5567de3b442484bd6a50
Dan Carley <dan.carley@gmail.com>
2015-01-16 15:12:18 +0000

    Fix rspec2 deprecated syntax in example tests
    expect should be used instead of should.

--- spec/ubuntu_unused_kernels_spec.rb
+++ spec/ubuntu_unused_kernels_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe UbuntuUnusedKernels do
   it 'should have a version number' do
-    UbuntuUnusedKernels::VERSION.should_not be_nil
+    expect(UbuntuUnusedKernels::VERSION).to_not be_nil
   end
 
   it 'should do something useful' do
-    false.should eq(true)
+    expect(false).to eq(true)
   end
 end
