1f0a2b5cf9991f25b765387bad5a0a080d760905
Dan Carley <dan.carley@gmail.com>
2015-03-18 22:35:03 +0000

    Add first failing tests for to_remove
    Which should take a list of packages from `get_installed()` and remove both
    the version returned by `get_current()` and the latest (if they're not the
    same).
    
    This is based on an existing utility that doesn't have any tests. I'm
    planning to open source this as a proper gem and by having tests we can have
    more confidence that it's doing the right thing and must trust to run it
    from cron instead of manually.
    
    - gds/puppet:modules/govuk/files/bin/govuk_unused_kernels
    
    The only difference at this stage is that it doesn't support the different
    suffix used by Ubuntu Lucid because we're not supporting that anymore.

--- lib/ubuntu_unused_kernels.rb
+++ lib/ubuntu_unused_kernels.rb
@@ -1,5 +1,18 @@
 require "ubuntu_unused_kernels/version"
 
 module UbuntuUnusedKernels
-  # Your code goes here...
+  class << self
+    def to_remove
+      packages = get_installed
+      return packages
+    end
+
+    def get_installed
+      return []
+    end
+
+    def get_current
+      return ''
+    end
+  end
 end
--- spec/ubuntu_unused_kernels_spec.rb
+++ spec/ubuntu_unused_kernels_spec.rb
@@ -5,7 +5,82 @@ describe UbuntuUnusedKernels do
     expect(UbuntuUnusedKernels::VERSION).to_not be_nil
   end
 
-  it 'should do something useful' do
-    expect(false).to eq(true)
+  describe 'to_remove' do
+    describe 'one kernel installed, is current and latest' do
+      it 'should return nothing' do
+        allow(subject).to receive(:get_current).with(no_args).and_return('3.13.0-43-generic')
+        allow(subject).to receive(:get_installed).with(no_args).and_return(%w{
+          linux-image-3.13.0-43-generic
+          linux-headers-3.13.0-43-generic
+        })
+
+        expect(subject.to_remove).to eq([])
+      end
+    end
+
+    describe 'five kernels installed' do
+      let(:installed) { %w{
+        linux-image-3.13.0-39-generic
+        linux-image-3.13.0-40-generic
+        linux-image-3.13.0-41-generic
+        linux-image-3.13.0-42-generic
+        linux-image-3.13.0-43-generic
+        linux-headers-3.13.0-39-generic
+        linux-headers-3.13.0-40-generic
+        linux-headers-3.13.0-41-generic
+        linux-headers-3.13.0-42-generic
+        linux-headers-3.13.0-43-generic
+      }}
+
+      describe 'current is latest' do
+        it 'should return everything except current/latest' do
+          allow(subject).to receive(:get_current).with(no_args).and_return('3.13.0-43-generic')
+          allow(subject).to receive(:get_installed).with(no_args).and_return(installed)
+
+          expect(subject.to_remove).to match_array(%w{
+            linux-image-3.13.0-39-generic
+            linux-image-3.13.0-40-generic
+            linux-image-3.13.0-41-generic
+            linux-image-3.13.0-42-generic
+            linux-headers-3.13.0-39-generic
+            linux-headers-3.13.0-40-generic
+            linux-headers-3.13.0-41-generic
+            linux-headers-3.13.0-42-generic
+          })
+        end
+      end
+
+      describe 'current is not latest' do
+        it 'should return everything except current and latest' do
+          allow(subject).to receive(:get_current).with(no_args).and_return('3.13.0-41-generic')
+          allow(subject).to receive(:get_installed).with(no_args).and_return(installed)
+
+          expect(subject.to_remove).to match_array(%w{
+            linux-image-3.13.0-39-generic
+            linux-image-3.13.0-40-generic
+            linux-image-3.13.0-42-generic
+            linux-headers-3.13.0-39-generic
+            linux-headers-3.13.0-40-generic
+            linux-headers-3.13.0-42-generic
+          })
+        end
+      end
+
+      describe 'unsorted list of kernels' do
+        it 'should return everything except current and latest' do
+          allow(subject).to receive(:get_current).with(no_args).and_return('3.13.0-41-generic')
+          allow(subject).to receive(:get_installed).with(no_args).and_return(installed.shuffle)
+
+          expect(subject.to_remove).to match_array(%w{
+            linux-image-3.13.0-39-generic
+            linux-image-3.13.0-40-generic
+            linux-image-3.13.0-42-generic
+            linux-headers-3.13.0-39-generic
+            linux-headers-3.13.0-40-generic
+            linux-headers-3.13.0-42-generic
+          })
+        end
+      end
+    end
   end
 end
