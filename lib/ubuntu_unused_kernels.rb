27a0da712ab2a47863ac00a7a60c986e9e7944ad
Dan Carley <dan.carley@gmail.com>
2015-03-19 20:17:45 +0000

    Implement removing latest in to_remove.

--- lib/ubuntu_unused_kernels.rb
+++ lib/ubuntu_unused_kernels.rb
@@ -1,9 +1,17 @@
 require "ubuntu_unused_kernels/version"
 
 module UbuntuUnusedKernels
+  PACKAGE_PREFIXES = %w{linux-image linux-headers}
+
   class << self
     def to_remove
       packages = get_installed
+
+      PACKAGE_PREFIXES.each do |prefix|
+        latest = packages.select { |package| package.start_with?(prefix) }.last
+        packages.delete(latest)
+      end
+
       return packages
     end
 
